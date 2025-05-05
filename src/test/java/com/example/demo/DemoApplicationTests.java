4de3c3375d2f082204703b16b218eb4b16edd880
sujalsharmaa <sujalsharma151@gmail.com>
2025-05-05 15:22:43 +0530

    fixed ci file

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
@@ -17,7 +17,7 @@ class DemoApplicationTests {
     void homePageLoads() {
         ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
         assertThat(response.getStatusCodeValue()).isEqualTo(200);
-        assertThat(response.getBody()).contains("Home Page");
+        assertThat(response.getBody()).contains("World Wonders");
     }
 
     @Test
